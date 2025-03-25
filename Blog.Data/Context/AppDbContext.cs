0229c17e13bf5932854c2715d97d66aed267d2b1
Sametcl <samet2016.2022@gmail.com>
2025-03-25 16:19:36 +0300

    Add project files.

---
+++ Blog.Data/Context/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Blog.Entity.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog.Data.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Article> Articles { get; set; }
+        public DbSet<Image> Images { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
