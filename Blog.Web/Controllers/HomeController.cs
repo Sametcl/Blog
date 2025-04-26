b000fadae4b52b18f3791a10dfa07fab95967bcd
Sametcl <samet2016.2022@gmail.com>
2025-04-26 23:37:05 +0300

    mapping articles and visitors to articlevisitors

--- Blog.Web/Controllers/HomeController.cs
+++ Blog.Web/Controllers/HomeController.cs
@@ -1,3 +1,5 @@
+using Blog.Data.UnitOfWorks;
+using Blog.Entity.Entities;
 using Blog.Service.Services.Abstraction;
 using Blog.Web.Models;
 using Microsoft.AspNetCore.Mvc;
@@ -9,11 +11,15 @@ namespace Blog.Web.Controllers
     {
         private readonly ILogger<HomeController> _logger;
         private readonly IArticleService articleService;
+        private readonly IHttpContextAccessor httpContextAccessor;
+        private readonly IUnitOfWork unitOfWork;
 
-        public HomeController(ILogger<HomeController> logger, IArticleService articleService)
+        public HomeController(ILogger<HomeController> logger, IArticleService articleService, IHttpContextAccessor httpContextAccessor, IUnitOfWork unitOfWork)
         {
             _logger = logger;
             this.articleService = articleService;
+            this.httpContextAccessor = httpContextAccessor;
+            this.unitOfWork = unitOfWork;
         }
 
 
@@ -46,8 +52,29 @@ namespace Blog.Web.Controllers
 
         public async Task<IActionResult> Detail(Guid articleId)
         {
-            var article = await articleService.GetArticleWithCategoryNonDeletedAsync(articleId);
-            return View(article);
+            var ipAddress = httpContextAccessor.HttpContext.Connection.RemoteIpAddress.MapToIPv4().ToString();
+            var articleVisitors = await unitOfWork.GetRepository<ArticleVisitor>().GetAllAsync(null, x => x.Visitor, y => y.Article);
+            var article = await unitOfWork.GetRepository<Article>().GetAsync(x => x.Id == articleId);
+
+
+            var result = await articleService.GetArticleWithCategoryNonDeletedAsync(articleId);
+
+            var visitor = await unitOfWork.GetRepository<Visitor>().GetAsync(x => x.IpAddress == ipAddress);
+
+            var addArticleVisitors = new ArticleVisitor(article.Id, visitor.Id);
+
+            if (articleVisitors.Any(x => x.VisitorId == addArticleVisitors.VisitorId && x.ArticleId == addArticleVisitors.ArticleId))
+            {
+                return View(result);
+            }
+            else
+            {
+                await unitOfWork.GetRepository<ArticleVisitor>().AddAsync(addArticleVisitors);
+                article.ViewCount+=1;
+                await unitOfWork.GetRepository<Article>().UpdateAsync(article);
+                await unitOfWork.SaveAsync();
+            }
+            return View(result);
         }
     }
 }
